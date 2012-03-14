86602a467dd4f3a3c5fa2a3436eaa86f7869ed8a
Andrew M. C. Dawes <dawes@pacificu.edu>
2012-03-14 06:56:25 -0700

    working cntrprop.py

--- python/cntrprop.py
+++ python/cntrprop.py
@@ -1,3 +1,5 @@
+from numpy import pi, zeros, arange, meshgrid, exp, sqrt 
+from numpy import arctan as atan
 L = 1.0
 w0 = 1.0
 sigma = 400
@@ -19,7 +21,7 @@ switchon = 60
 
 relaxTransits = 5
 transitsCompleted = 0
-integratesteps = (NZ-1)*(relaxTransits + transits)
+integrateSteps = (NZ-1)*(relaxTransits + transits)
 zstep = L/(NZ-1)
 xstep = 4.0*w0/N
 ystep = 0.99*xstep
@@ -39,17 +41,46 @@ expDz = zeros((N,N))
 expDzByTwo = zeros((N,N))
 noise = zeros((N,N))
 
-x = np.arange(-N/2, N/2, 1)
-y = np.arange(-N/2, N/2, 1)
-i, j = np.meshgrid(x, y)
+x = arange(-N/2, N/2, 1)
+y = arange(-N/2, N/2, 1)
+i, j = meshgrid(x, y)
 
 theta_n = zstep/(2*sigma) * ((i*kxstep)**2 + (j*kystep)**2)
 expDz = exp(-1j * theta_n)
 expDzByTwo = exp(-1j * theta_n/2.0)
 
+def spot_size (z, z0, w0):
+	return w0 * sqrt(1 + (z/z0)*(z/z0) )
+
+def radius_curvature (z, z0):
+	# This could be smarter, just adding epsilon to avoid nan's
+	if (z == 0): 
+		z += 1e-31
+	return z * ( 1 + (z0/z)*(z0/z) )
+
+def guoy_phase (z, z0):
+	return atan(z/z0)
+
+def rayleigh_range (w0, wavelambda):
+	return pi*w0*w0/wavelambda;
+
+def gaussian_beam (x, y, z, z0, w0, k):
+	r = sqrt(x*x + y*y)
+	w = spot_size(z,z0,w0)
+	R = radius_curvature(z,z0)
+	eta = guoy_phase(z,z0)
+  # return w0/w * exp(- r*r/(w*w)) * exp(-I*k*z - I*k*r*r/(2*R) + I*eta);
+  # The exp(ikz) term in the previous definition, causes extra phase accumulation
+  # compared to the BPM. I need to sort this out for sure. Right now, leave it off.
+	return w0/w * exp(- r*r/(w*w)) * exp(- 1j*k*r*r/(2*R) + 1j*eta)
+
+
+
 # do something to add noise
 
 for intstep in range(integrateSteps):
-	print "integration step %d of %d" % (intstep, integrationSteps)
-	f = sqrt(IL) * ramp * gaussian_beam(x - pumpoffset[0], y - pumpoffset[1], 0, L*sigma/2, w0, L*sigma/(w0*w0) * exp(1j*(bkin[0]*x + bkin[1]*y)) + sqrt(IL)*ramp*
+	print "integration step %d of %d" % (intstep, integrateSteps)
+	f[:,:,0] = sqrt(IL) * ramp * gaussian_beam(x - pumpoffset[0], y - pumpoffset[1], 0, L*sigma/2, w0, L*sigma/(w0*w0)) * exp(1j*(bkin[0]*x + bkin[1]*y)) + sqrt(IL)*ramp*probeamp*exp(- (x*x + y*y) / ((w0*probewidth)*(w0*probewidth)))*exp(1j*kin*x) # + ramp*noiseamp*noise(i,j) #TODO restore noise term
+	b[:,:,NZ-1] = sqrt(IL) * ramp * gaussian_beam(x+pumpoffset[0], y+pumpoffset[1], 0, L*sigma/2, w0, L*sigma/(w0*w0))
+
 
