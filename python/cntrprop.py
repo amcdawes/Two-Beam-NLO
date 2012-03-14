efdf794e040c74f70f4722333dceec9e3bd860fe
Andrew M. C. Dawes <dawes@pacificu.edu>
2012-03-14 07:47:04 -0700

    starting to implement the integration

--- python/cntrprop.py
+++ python/cntrprop.py
@@ -1,4 +1,4 @@
-from numpy import pi, zeros, arange, meshgrid, exp, sqrt 
+from numpy import pi, zeros, arange, meshgrid, exp, sqrt, conjugate
 from numpy import arctan as atan
 L = 1.0
 w0 = 1.0
@@ -31,19 +31,22 @@ lbynzeta = L / NZ * eta
 stpp = 1j*lbynzeta
 ramp = 1.0
 
-f = zeros((N,N,NZ))
-b = zeros((N,N,NZ))
-f_tmp = zeros((N,N,NZ))
-b_tmp = zeros((N,N,NZ))
-ff = zeros((N,N))
-bb = zeros((N,N))
-expDz = zeros((N,N))
-expDzByTwo = zeros((N,N))
+f = zeros((N,N,NZ)).astype(complex)
+b = zeros((N,N,NZ)).astype(complex)
+f_tmp = zeros((N,N,NZ)).astype(complex)
+b_tmp = zeros((N,N,NZ)).astype(complex)
+probe = zeros((N,N)).astype(complex)
+ff = zeros((N,N)).astype(complex)
+bb = zeros((N,N)).astype(complex)
+expDz = zeros((N,N)).astype(complex)
+expDzByTwo = zeros((N,N)).astype(complex)
 noise = zeros((N,N))
 
 x = arange(-N/2, N/2, 1)
 y = arange(-N/2, N/2, 1)
 i, j = meshgrid(x, y)
+x = i*xstep # set master coordinate grid
+y = j*ystep
 
 theta_n = zstep/(2*sigma) * ((i*kxstep)**2 + (j*kystep)**2)
 expDz = exp(-1j * theta_n)
@@ -74,13 +77,32 @@ def gaussian_beam (x, y, z, z0, w0, k):
   # compared to the BPM. I need to sort this out for sure. Right now, leave it off.
 	return w0/w * exp(- r*r/(w*w)) * exp(- 1j*k*r*r/(2*R) + 1j*eta)
 
-
+probe = sqrt(IL)*ramp*probeamp*exp(- (x*x + y*y) / ((w0*probewidth)*(w0*probewidth)))*exp(1j*kin*x)
 
 # do something to add noise
 
 for intstep in range(integrateSteps):
 	print "integration step %d of %d" % (intstep, integrateSteps)
-	f[:,:,0] = sqrt(IL) * ramp * gaussian_beam(x - pumpoffset[0], y - pumpoffset[1], 0, L*sigma/2, w0, L*sigma/(w0*w0)) * exp(1j*(bkin[0]*x + bkin[1]*y)) + sqrt(IL)*ramp*probeamp*exp(- (x*x + y*y) / ((w0*probewidth)*(w0*probewidth)))*exp(1j*kin*x) # + ramp*noiseamp*noise(i,j) #TODO restore noise term
+	f[:,:,0] = sqrt(IL) * ramp * gaussian_beam(x - pumpoffset[0], y - pumpoffset[1], 0, L*sigma/2, w0, L*sigma/(w0*w0)) * exp(1j*(bkin[0]*x + bkin[1]*y))# + ramp*noiseamp*noise(i,j) #TODO restore noise term
+	if intstep > switchon: 
+		f[:,:,0] += probe
 	b[:,:,NZ-1] = sqrt(IL) * ramp * gaussian_beam(x+pumpoffset[0], y+pumpoffset[1], 0, L*sigma/2, w0, L*sigma/(w0*w0))
+
+	# step forward at the first point
+	fi = f[:,:,0]*conjugate(f[:,:,0])
+	bi = b[:,:,0]*conjugate(b[:,:,0])
+	ff = f[:,:,0] * exp(stpp*(fi+2.0*bi))
+
+	# step backward at the last points:
+	fi = f[:,:,-1]*conjugate(f[:,:,-1])
+	bi = b[:,:,-1]*conjugate(b[:,:,-1])
+	bb = b[:,:,-1] * exp(stpp*(2.0*fi+bi))
+
+	# Fourier transform the fields
+
+	# Free-space propagate half-step
+
+	# inverse Fourier
+
 
 
